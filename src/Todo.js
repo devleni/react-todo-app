7e23e44d5d459ead5321db669040cb7c9c6f1d42
devleni <50588358+devleni@users.noreply.github.com>
2020-02-25 16:16:50 +0100

    add editing functionality

--- src/Todo.js
+++ src/Todo.js
@@ -3,19 +3,52 @@ import React, { Component } from "react";
 class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    };
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+  handleUpdate(event) {
+    event.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
   render() {
-    return (
-      <div className="ba br3 pa2 ma2 bg-washed-blue">
-        <button>Edit</button>
-        <button onClick={this.handleRemove}>X</button>
-        <li className="fl w-70">{this.props.task}</li>
-      </div>
-    )
+    let result;
+    if(this.state.isEditing) {
+      result = (
+        <div>
+          <form onSubmit={this.handleUpdate}>
+            <input type="text" value={this.state.task} name="task" onChange={this.handleChange} />
+            <button>Save</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div className="ba br3 pa2 ma2 bg-washed-blue">
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleRemove}>X</button>
+          <li className="fl w-70">{this.props.task}</li>
+        </div>
+      );
+    }
+    return result;
   }
 }
 
